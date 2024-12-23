6c044a7010ebf6736049f624269f5d509de65442
ray-anon <siddikshetty12@gmail.com>
2024-12-23 22:37:29 +0530

    first task of this sha256 is done

--- hash.cpp
+++ hash.cpp
@@ -8,6 +8,7 @@ void deleting_extra_calculated_prime(vector<int> &prime64)
     }
 
 }
+//estimating the range for finding first 64 primes
 int estimate_range(int n)
 {
     double x0 = n * log(n);
@@ -21,6 +22,7 @@ int estimate_range(int n)
     }
     return int(x1);
 }
+
 vector<int>  find_first_64_prime()
 {
     vector<int> v;
@@ -57,22 +59,40 @@ int main()
     int n = password.size();
     int arr[n];
 
+    //converting string to ascii value
     for(int i = 0; i < n; i++)
     {
         arr[i] = int(password[i]);
     }
 
+    //finding first 64 prime
     vector<int> prime64 = find_first_64_prime();
 
+    //deleting extra calculated primes in the estimated range
     deleting_extra_calculated_prime(prime64);
 
 
-    vector<double> cuberoot64;
-
+    vector<double> hexprime64;
+    vector<string> hexvalues;
+    //finding cuberoot of first 64 primes
     for(int i = 0; i < prime64.size(); i++)
     {
-        cuberoot64.push_back(cbrt(prime64[i]));
+        hexprime64.push_back(cbrt(prime64[i]));
     }
-    cout<< cuberoot64[0];
-
+    
+    //deleting the integer part and only having the fractional part
+    for(int i = 0; i < hexprime64.size(); i++)
+    {
+        hexprime64[i] = hexprime64[i] - int(hexprime64[i]);
+    }
+    
+    // storing hex values and using stringstreams and unsigned int to convert decimal values like 1.1191e+9 to normal integer 119100000
+    for(int i = 0; i < hexprime64.size(); i++)
+    {
+        unsigned int num =  static_cast<unsigned int>(hexprime64[i] * pow(2 , 32));
+        stringstream ss ;
+        ss << hex << num;
+        hexvalues.push_back("0x" + ss.str());
+    }
+    
 }
---
+++ test.cpp
@@ -0,0 +1,7 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    bitset<9> b();
+    cout<< b;
+}
